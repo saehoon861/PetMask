3caacf56e50be10e854c946dccf39247c13d673c
kim sehoon <qazxcv39372046@gmail.com>
2026-06-01 15:28:52 +0900

    초기 모델 생성

---
+++ src/model/model.py
@@ -0,0 +1,88 @@
+import torch.nn as nn
+import torchvision.models
+
+
+def convrelu(in_channels, out_channels, kernel, padding):
+  return nn.Sequential(
+    nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+    nn.ReLU(inplace=True),
+  )
+
+
+class ResNetUNet(nn.Module):
+  def __init__(self, n_class):
+    super().__init__()
+
+    self.base_model = torchvision.models.resnet18(pretrained=True)
+    self.base_layers = list(self.base_model.children())
+
+    self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
+
+    #layer0_1x1: 단순히 채널을 섞는 것뿐 아니라, ReLU를 통해 음수 값을 자르고 새로운 표현을 만들 (resnet를 인코더처럼 사용하기 때문에 추가한 레이어)
+    self.layer0_1x1 = convrelu(64, 64, 1, 0)
+    self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 64, x.H/4, x.W/4)
+    self.layer1_1x1 = convrelu(64, 64, 1, 0)
+    self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
+    self.layer2_1x1 = convrelu(128, 128, 1, 0)
+    self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
+    self.layer3_1x1 = convrelu(256, 256, 1, 0)
+    self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+    self.layer4_1x1 = convrelu(512, 512, 1, 0)
+
+#scale_factor: 공간 크기를 몇 배로 키울지
+#mode:어떤 보간법으로 값을 채울지
+#align_corners:입력 feature map의 모서리 픽셀과 출력 feature map의 모서리 픽셀을 정확히 맞출지를 정하는 옵션
+#여기서는 transposed conv를 하지 않음
+#요즘은 transposed conv보다는 보간을 활용한 upsampling 활용하는 추세
+#이유: 구현 쉬움, checkerboard artifact적음
+    self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+    self.conv_up3 = convrelu(256 + 512, 512, 3, 1)
+    self.conv_up2 = convrelu(128 + 512, 256, 3, 1)
+    self.conv_up1 = convrelu(64 + 256, 256, 3, 1)
+    self.conv_up0 = convrelu(64 + 256, 128, 3, 1)
+
+    self.conv_original_size0 = convrelu(3, 64, 3, 1)
+    self.conv_original_size1 = convrelu(64, 64, 3, 1)
+    self.conv_original_size2 = convrelu(64 + 128, 64, 3, 1)
+
+    self.conv_last = nn.Conv2d(64, n_class, 1)
+
+  def forward(self, input):
+    x_original = self.conv_original_size0(input)
+    x_original = self.conv_original_size1(x_original)
+
+    layer0 = self.layer0(input)
+    layer1 = self.layer1(layer0)
+    layer2 = self.layer2(layer1)
+    layer3 = self.layer3(layer2)
+    layer4 = self.layer4(layer3)
+
+    layer4 = self.layer4_1x1(layer4)
+    x = self.upsample(layer4)
+    layer3 = self.layer3_1x1(layer3)
+    x = torch.cat([x, layer3], dim=1)
+    x = self.conv_up3(x)
+
+    x = self.upsample(x)
+    layer2 = self.layer2_1x1(layer2)
+    x = torch.cat([x, layer2], dim=1)
+    x = self.conv_up2(x)
+
+    x = self.upsample(x)
+    layer1 = self.layer1_1x1(layer1)
+    x = torch.cat([x, layer1], dim=1)
+    x = self.conv_up1(x)
+
+    x = self.upsample(x)
+    layer0 = self.layer0_1x1(layer0)
+    x = torch.cat([x, layer0], dim=1)
+    x = self.conv_up0(x)
+
+    x = self.upsample(x)
+    x = torch.cat([x, x_original], dim=1)
+    x = self.conv_original_size2(x)
+
+    out = self.conv_last(x)
+
+    return out
