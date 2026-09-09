783fbfe48d7fc1fb3e691aaba1b0d27d94b532f1
kim sehoon <qazxcv39372046@gmail.com>
2026-09-09 23:37:44 +0900

    feat: add segmentation page layout

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 from backend.routers import router
 
-app = FastAPI(docs_url="/docs")
+
+app = FastAPI()
 
 app.include_router(router)
+
+app.mount(
+    "/frontend",
+    StaticFiles(directory="frontend"),
+    name="frontend"
+)
 
 
 @app.get("/")
 async def root():
     return FileResponse("frontend/index.html")
-
-
